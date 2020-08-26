0077046c276815fb96f303421ee8fd697474f325
AnaZermeno <anazermeno19@gmail.com>
2020-08-26 15:00:54 -0600

    Created source files and added sidemenu jsx file

---
+++ tloa/src/components/sidemenu.jsx
@@ -0,0 +1,11 @@
+import React, { cComponent } from 'react'
+
+export default class Sidemenu extends Component {
+    render () {
+        return (
+            <div>
+                <p>holaaa</p>
+            </div>
+        )
+    }
+}
