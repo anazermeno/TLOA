0a7e327f12e0d50038737238e031490114d8b6c5
AnaZermeno <anazermeno19@gmail.com>
2020-08-26 19:21:08 -0600

    Erased dafult image and added base html structure to jsx about file

--- tloa/src/App.js
+++ tloa/src/App.js
@@ -1,15 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
 import Sidemenu from './components/sidemenu';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        
         <Sidemenu></Sidemenu>
-      </header>
     </div>
   );
 }
---
+++ tloa/src/components/about.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import './styles.css';
+
+export default class About extends Component{
+    render (){
+        return (
+            <html>
+                <head>
+
+                </head>
+                <body>
+                    
+                </body>
+            </html>
+        )
+    }
+}
