ba8192b73f60ecb822fc8ef81510d981e5a563c9
AnaZermeno <anazermeno19@gmail.com>
2020-08-27 22:40:05 -0600

    Erased html tags from jsx files

--- tloa/src/components/about.jsx
+++ tloa/src/components/about.jsx
@@ -4,14 +4,11 @@ import './styles.css';
 export default class About extends Component{
     render (){
         return (
-            <div>
-                <head>
 
-                </head>
-                <body id="body_about">
+                <div id="body_about">
                     <h1>Welcome to my page!</h1>
-                </body>
-            </div>
+                </div>
+
         )
     }
 }
--- tloa/src/components/sidemenu.jsx
+++ tloa/src/components/sidemenu.jsx
@@ -5,18 +5,14 @@ export default class Sidemenu extends Component {
     render () {
         return (
             <div>
-                <head>
-                    <meta name="viewport" content="width=device-width, initial-scale=1"/>
-                    <link rel="stylesheet" type="text/css" href="./styles.css"/>
-                </head>
-                <body id="body_navbar">
+                <div id="body_navbar">
                     <div class="sidenav">
                         <a href="#">About</a><br/>
                         <a href="#">Projects</a><br/>
                         <a href="#">Resources</a><br/>
                         <a href="#">Gallery</a><br/>
                     </div>
-                </body>
+                </div>
             </div>
         )
     }
