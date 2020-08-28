be327d3a6f8f936c35ee61cbda56fefa9e15a79f
AnaZermeno <anazermeno19@gmail.com>
2020-08-27 23:45:43 -0600

    about.jsx css style for h1

--- tloa/src/components/about.jsx
+++ tloa/src/components/about.jsx
@@ -6,9 +6,8 @@ import profilepic from './media/profilepic.jpeg';
 export default class About extends Component{
     render (){
         return (
-
                 <div id="body_about">
-                    <h1>Welcome to my page!</h1>
+                    <h1 id="title">Welcome to my page!</h1>
                     <img src={profilepic} id="img_about"/>
                     <h2>Hi there!</h2>
                     <p>My name is Ana, I'm studying Computer Science at Tec de Monterrey</p>
