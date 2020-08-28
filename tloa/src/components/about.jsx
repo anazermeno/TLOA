48af69566ad3d1fc442d2a8833c5e420839186a1
AnaZermeno <anazermeno19@gmail.com>
2020-08-27 23:13:49 -0600

    Added profile picture and its css

--- tloa/src/components/about.jsx
+++ tloa/src/components/about.jsx
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import './styles.css';
+import profilepic from './media/profilepic.jpeg'
+
 
 export default class About extends Component{
     render (){
@@ -7,8 +9,8 @@ export default class About extends Component{
 
                 <div id="body_about">
                     <h1>Welcome to my page!</h1>
+                    <img src={profilepic} id="img_about"/>
                 </div>
-
         )
-    }
+    } 
 }
